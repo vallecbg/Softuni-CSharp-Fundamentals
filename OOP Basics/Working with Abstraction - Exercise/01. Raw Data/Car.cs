225570676270670301463ae10b5d477b6c3e5544
vallecbg <valioplayofficial@gmail.com>
2019-03-21 21:46:52 +0200

    solution

---
+++ OOP Basics/Working with Abstraction - Exercise/01. Raw Data/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    class Car
+    {
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, double tire1Pressure, int tire1Age, double tire2Pressure, int tire2Age, double tire3Pressure, int tire3Age, double tire4Pressure, int tire4Age)
+        {
+            this.Model = model;
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+            this.CargoWeight = cargoWeight;
+            this.CargoType = cargoType;
+            this.Tires = new KeyValuePair<double, int>[] { KeyValuePair.Create(tire1Pressure, tire1Age), KeyValuePair.Create(tire2Pressure, tire2Age), KeyValuePair.Create(tire3Pressure, tire3Age), KeyValuePair.Create(tire4Pressure, tire4Age) };
+        }
+        public string Model;
+        public int EngineSpeed;
+        public int EnginePower;
+        public int CargoWeight;
+        public string CargoType;
+        public KeyValuePair<double, int>[] Tires;
+    }
+}
